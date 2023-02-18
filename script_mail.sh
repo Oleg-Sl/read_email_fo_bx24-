8d3c840c808e0450356da0006c8b7840d8423710
Oleg <slepcov_oleg@mail.ru>
2023-02-19 01:22:01 +0700

    Add readme

--- script_mail.sh
+++ script_mail.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-MY_WORK_DIR="/home/oleg/PycharmProjects/bits/mail"
+MY_WORK_DIR="/home/atonlab/projects/read_email_fo_bx24-"
 
 source "$MY_WORK_DIR/venv/bin/activate"
 cd "$MY_WORK_DIR"
